d0c9e021e3dcda597192c6fd9b3ed5b1cd6c2973
Glitch (glitch-hello-node) <none>
2021-05-12 12:36:48 +0000

    🎮👌 Checkpoint ./server.js:255927/2873 ./crypto.js:255927/11243

--- crypto.js
+++ crypto.js
@@ -2,6 +2,9 @@ const crypto = require("crypto");
 
 const algorithm = "aes-192-cbc";
 
+const encode = data => data.replace(/\+/g, "-");
+const decode = data => data.replace(/-/g, "+");
+
 exports.encrypt = data =>
   new Promise((resolve, reject) =>
     // First, we'll generate the key. The key length is dependent on the algorithm.
@@ -22,10 +25,15 @@ exports.encrypt = data =>
         const cipher = crypto.createCipheriv(algorithm, key, iv);
 
         let encrypted = "";
-        cipher.setEncoding("hex");
+        cipher.setEncoding("base64");
 
         cipher.on("data", chunk => (encrypted += chunk));
-        cipher.on("end", () => resolve([encrypted, iv]));
+        cipher.on("end", () =>
+          resolve([
+            encode(encrypted),
+            encode(Buffer.from(iv).toString("base64"))
+          ])
+        );
 
         cipher.write(data);
         cipher.end();
@@ -33,7 +41,7 @@ exports.encrypt = data =>
     })
   );
 
-exports.decrypt = data =>
+exports.decrypt = (encrypted, iv) =>
   new Promise((resolve, reject) => {
     crypto.scrypt(process.env.PASSWORD, "salt", 24, (err, key) => {
       if (err) {
@@ -41,17 +49,11 @@ exports.decrypt = data =>
         return;
       }
 
-    // The IV is usually passed along with the ciphertext.
-    const iv = Buffer.alloc(16, 0); // Initialization vector.
+      const decipher = crypto.createDecipheriv(algorithm, key, Buffer.from(decode(iv), 'base64'));
 
-    const decipher = crypto.createDecipheriv(algorithm, key, iv);
-
-    // Encrypted using same algorithm, key and iv.
-    const encrypted =
-      "e5f79c5915c02171eec6b212d5520d44480993d7d622a7c4c2da32f6efda0ffa";
-    let decrypted = decipher.update(encrypted, "hex", "utf8");
-    decrypted += decipher.final("utf8");
-    console.log(decrypted);
-    // Prints: some clear text data
-    decipher.final([outputEncoding]);
+      // Encrypted using same algorithm, key and iv.
+      let decrypted = decipher.update(decode(encrypted), "base64", "utf8");
+      decrypted += decipher.final("utf8");
+      resolve(decrypted);
+    });
   });
--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT;
-const { encrypt } = require("./crypto");
+const { encrypt, decrypt } = require("./crypto");
 
+// ex: /?token=yW--sHWMTP80cCYbyu01KA==&token_iv=C5eSwdMxVPA4nIpGAthyLg==&id=LN88YRtw0Dc3pM/Wf3OKiQ==&id_iv=LJB4OrN5kzZrNqkfKyQmTw==
+app.get("/", async (req, res) => {
+  const { token: encryptedToken, token_iv, id: encryptedId, id_iv } = req.query;
+  if (!encryptedToken || !token_iv || !encryptedId || !id_iv) {
+    res.send(400, "Invalid request");
+    return;
+  }
+  const [token, id] = await Promise.all(
+    [decrypt(encryptedToken, token_iv), decrypt(encryptedId, id_iv)]
+  );
+  res.send(
+    `Token: ${token}, ID: ${id}`
+  );
+})
 app.get("/encrypt", async (req, res) => {
   const { token, id } = req.query;
   if (!token || !id) {
@@ -12,7 +26,9 @@ app.get("/encrypt", async (req, res) => {
   const [[encryptedToken, tokenIv], [encryptedId, idIv]] = await Promise.all(
     [token, id].map(encrypt)
   );
-  res.send(`/?token=${encryptedToken}&token, ID: ${encryptedId}`);
+  res.send(
+    `/?token=${encryptedToken}&token_iv=${tokenIv}&id=${encryptedId}&id_iv=${idIv}`
+  );
 });
 
 app.listen(port, () => {
