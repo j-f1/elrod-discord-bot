2fcc8f1d89ed8ef1996ac3cc762207129d817f5a
Glitch (glitch-hello-node) <none>
2021-05-12 12:25:47 +0000

    ☄️💑 Checkpoint ./crypto.js:255927/10606 ./server.js:255927/1668

--- crypto.js
+++ crypto.js
@@ -1,26 +1,57 @@
-const crypto = require('crypto');
+const crypto = require("crypto");
 
-const algorithm = 'aes-192-cbc';
-const password = 'Password used to generate key';
+const algorithm = "aes-192-cbc";
 
-// First, we'll generate the key. The key length is dependent on the algorithm.
-// In this case for aes192, it is 24 bytes (192 bits).
-crypto.scrypt(password, 'salt', 24, (err, key) => {
-  if (err) throw err;
-  // Then, we'll generate a random initialization vector
-  crypto.randomFill(new Uint8Array(16), (err, iv) => {
-    if (err) throw err;
+exports.encrypt = data =>
+  new Promise((resolve, reject) =>
+    // First, we'll generate the key. The key length is dependent on the algorithm.
+    // In this case for aes192, it is 24 bytes (192 bits).
+    crypto.scrypt(process.env.PASSWORD, "salt", 24, (err, key) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      // Then, we'll generate a random initialization vector
+      crypto.randomFill(new Uint8Array(16), (err, iv) => {
+        if (err) {
+          reject(err);
+          return;
+        }
 
-    // Once we have the key and iv, we can create and use the cipher...
-    const cipher = crypto.createCipheriv(algorithm, key, iv);
+        // Once we have the key and iv, we can create and use the cipher...
+        const cipher = crypto.createCipheriv(algorithm, key, iv);
 
-    let encrypted = '';
-    cipher.setEncoding('hex');
+        let encrypted = "";
+        cipher.setEncoding("hex");
 
-    cipher.on('data', (chunk) => encrypted += chunk);
-    cipher.on('end', () => console.log(encrypted));
+        cipher.on("data", chunk => (encrypted += chunk));
+        cipher.on("end", () => resolve([encrypted, iv]));
 
-    cipher.write('some clear text data');
-    cipher.end();
+        cipher.write(data);
+        cipher.end();
+      });
+    })
+  );
+
+exports.decrypt = data =>
+  new Promise((resolve, reject) => {
+    crypto.scrypt(process.env.PASSWORD, "salt", 24, (err, key) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+
+    // The IV is usually passed along with the ciphertext.
+    const iv = Buffer.alloc(16, 0); // Initialization vector.
+
+    const decipher = crypto.createDecipheriv(algorithm, key, iv);
+
+    // Encrypted using same algorithm, key and iv.
+    const encrypted =
+      "e5f79c5915c02171eec6b212d5520d44480993d7d622a7c4c2da32f6efda0ffa";
+    let decrypted = decipher.update(encrypted, "hex", "utf8");
+    decrypted += decipher.final("utf8");
+    console.log(decrypted);
+    // Prints: some clear text data
+    decipher.final([outputEncoding]);
   });
-});
--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT;
+const { encrypt } = require("./crypto");
 
-app.get("/", (req, res) => {
+app.get("/encrypt", async (req, res) => {
   const { token, id } = req.query;
   if (!token || !id) {
     res.send(400, "Invalid request");
     return;
   }
-  res.send(`Token: ${token}, ID: ${id}`);
+  const [[encryptedToken, tokenIv], [encryptedId, idIv]] = await Promise.all(
+    [token, id].map(encrypt)
+  );
+  res.send(`/?token=${encryptedToken}&token, ID: ${encryptedId}`);
 });
 
 app.listen(port, () => {
